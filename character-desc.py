167f4e01c335c0004071138131eed0e192be5937
Ricky <94165274+RickyOutofLine@users.noreply.github.com>
2025-11-13 09:07:24 +0800

    Add files via upload
    3 characters pa boss

---
+++ character-desc.py
@@ -0,0 +1,22 @@
+#Kingdom of [idkthename]
+
+#Fire Wizard
+
+fire_wizard_desc = """
+Archmage [Fire Wizard Name], had always been the Master of Flame Magic. Wielding the power of fire to bring enemies into the curse of intense heat, may they be lost to all but ashes, or to be devoured by the monsters of their sin.
+With a flick of the wrist, the Fire Wizard can conjure blazing fireballs, create walls of flame, and summon fiery storms that engulf the battlefield. Their mastery over fire allows them to incinerate foes, melt obstacles, and light the darkest of paths.
+Clad in robes of ember and ash, the kingdom sees the Fire Wizard as a beacon of destruction and renewal, a force that can both annihilate and in a sense, purify. A testament to their fiery prowess."""
+
+#Fire Knight
+
+fire_knight_desc = """
+Sir [Fire Knight Name], also known as the Flame Guardian, the kingdom's valiant warrior who has mastered the art of combat. 
+With the combined efforts of Archmage [Fire Wizard Name] and the kingdom's master blacksmith, the Fire Knight wore a suit of enchanted armor forged with the scales 
+of the dragons that once terrorized the land. And with the fiery sword blazing with the power of the fire imbued from the archmage's hands, 
+the Fire Knight became an unstoppable force on the battlefield, striking doom upon the lands, piercing upon the honor of his kingdom's enemies."""
+
+#Desert Faction
+
+wind_hasashin_desc = """ From the vast deserts of the [desert royalty/capital], the Wind Hasashin are elite assassins who have mastered the art of stealth and agility. Enduring the harsh
+winds and scorching sands, they have honed their skills to perfection, striking swiftly and silently from the surges of desert gales. From  the fabrics and sharp daggers of their remote kingdom, [desert royalty/capital],
+they fight in groups, using the desert winds to their advantage, shrinking the sanity of their enemies as they vanish as quickly as their daggers pass through the bodies of their foes. """
